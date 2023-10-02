4fb0d9d004959420f95b2700aa60fd0b83b56099
Bruno de Araújo Alves <me@baraus.dev>
2023-10-02 10:34:50 -0300

    docs: added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('KingSystem API')
+    .setDescription('A KingSystem API to control your system')
+    .setVersion('v1')
+    .addTag('default')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.APP_PORT || 3000);
 }
+
 bootstrap();
