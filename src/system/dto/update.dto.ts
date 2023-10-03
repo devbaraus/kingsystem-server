399ae891f19cf471c36c8cb8c8cf5c8b2c0fa9e4
Bruno de Araújo Alves <me@baraus.dev>
2023-10-03 00:43:16 -0300

    doc: improved documentation

--- src/system/dto/update.dto.ts
+++ src/system/dto/update.dto.ts
@@ -1,38 +1,41 @@
 import {
   IsEmail,
   IsEnum,
+  IsNotEmpty,
   IsOptional,
   IsString,
   IsUrl,
   MaxLength,
 } from 'class-validator';
 import { SystemStatus } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateSystemDto {
-  // @IsString()
-  // @IsNotEmpty()
-  // @MaxLength(10)
-  // acronym: string;
-  //
-  // @IsString()
-  // @IsNotEmpty()
-  // @MaxLength(100)
-  // description: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
+  acronym: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  description: string;
 
   @IsOptional()
   @IsEmail()
   @MaxLength(100)
-  email?: string;
+  email: string;
 
   @IsOptional()
   @IsUrl()
   @MaxLength(50)
-  url?: string;
+  url: string;
 
   @IsString()
   @MaxLength(500)
   reason: string;
 
   @IsEnum(SystemStatus)
+  @ApiProperty({ enum: SystemStatus })
   status: SystemStatus;
 }
