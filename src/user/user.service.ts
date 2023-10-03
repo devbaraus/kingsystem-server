7aaeed07e4cb06b115e1c67aa38a92e2109fd569
Bruno de Araújo Alves <me@baraus.dev>
2023-10-03 00:38:46 -0300

    fix: removed unused services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,41 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { UpdateUserDto } from './dto';
-import { PaginateQueryDto } from '../dto/paginate-query.dto';
-import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
-
-  async findManyAndPaginate(query: PaginateQueryDto, pathname: string) {
-    try {
-      const data = await this.prisma.paginate<User>(
-        this.prisma.user,
-        query,
-        pathname,
-      );
-
-      const results = data?.results?.map((user) => {
-        return {
-          ...user,
-          passwordHash: undefined,
-        };
-      });
-
-      return {
-        ...data,
-        results,
-      };
-    } catch (err) {
-      console.log(err);
-      throw new BadRequestException('Something went wrong');
-    }
-  }
-
-  async findOne() {
-    return 'findOne';
-  }
 
   async update(userId: number, dto: UpdateUserDto) {
     const user = await this.prisma.user.update({
@@ -51,17 +20,5 @@ export class UserService {
       ...user,
       passwordHash: undefined,
     };
-  }
-
-  async delete(userId: number) {
-    try {
-      await this.prisma.user.delete({
-        where: {
-          id: userId,
-        },
-      });
-    } catch (e) {
-      console.log(e);
-    }
   }
 }
