0c7e42613014689e3c384025cee3e1a78ee4899b
Bruno de Araújo Alves <me@baraus.dev>
2023-10-03 00:47:04 -0300

    doc: improved auth doc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,51 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+import { AuthDto, SignAuthDto } from './dto';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Sign up user' })
+  @ApiCreatedResponse({
+    description: 'created',
+    type: AuthDto,
+  })
+  @ApiConflictResponse({
+    description: 'conflict',
+  })
+  @ApiBadRequestResponse({
+    description: 'bad request',
+  })
   @Post('signup')
-  async signUp(@Body() body: AuthDto) {
+  async signUp(@Body() body: SignAuthDto) {
     return this.authService.signUp(body);
   }
 
+  @ApiOperation({ summary: 'Sign in user' })
+  @ApiOkResponse({
+    description: 'success',
+    type: AuthDto,
+  })
+  @ApiForbiddenResponse({
+    description: 'forbidden',
+  })
+  @ApiBadRequestResponse({
+    description: 'bad request',
+  })
   @HttpCode(HttpStatus.OK)
   @Post('signin')
-  async signIn(@Body() body: AuthDto) {
+  async signIn(@Body() body: SignAuthDto) {
     return this.authService.signIn(body);
   }
 }
