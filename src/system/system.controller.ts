0adc2ec1aa05c27b50c1c44c4076e1365bf70d9a
Bruno de Araújo Alves <me@baraus.dev>
2023-10-03 00:43:55 -0300

    doc: improved system documentation

--- src/system/system.controller.ts
+++ src/system/system.controller.ts
@@ -3,32 +3,63 @@ import {
   Controller,
   Get,
   Param,
-  Patch,
   Post,
+  Put,
   Query,
   Req,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { SystemService } from './system.service';
 import { CreateSystemDto, UpdateSystemDto } from './dto';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
-import { PaginateQueryDto } from '../dto/paginate-query.dto';
+import { PaginationQueryDto } from '../dto/pagination-query.dto';
 import { Request } from 'express';
+import { PaginationDto } from '../dto';
+import { SystemDto } from './dto/system.dto';
 
 @UseGuards(JwtGuard)
 @ApiTags('system')
+@ApiUnauthorizedResponse({
+  description: 'unauthorized',
+})
+@ApiBadRequestResponse({
+  description: 'bad request',
+})
 @Controller('system')
 export class SystemController {
   constructor(private readonly service: SystemService) {}
 
+  @ApiOperation({ summary: 'Create system' })
+  @ApiCreatedResponse({
+    description: 'created',
+    type: SystemDto,
+  })
+  @ApiConflictResponse({
+    description: 'conflict',
+  })
   @Post('')
   async create(@GetUser('id') userId: number, @Body() dto: CreateSystemDto) {
     return this.service.create(dto, userId);
   }
 
-  @Patch(':id')
+  @ApiOperation({ summary: 'Update system' })
+  @ApiOkResponse({
+    description: 'success',
+    type: SystemDto,
+  })
+  @Put(':id')
   async update(
     @Param('id') id: string,
     @GetUser('id') userId: number,
@@ -38,15 +69,36 @@ export class SystemController {
     return this.service.update(id, dto, userId);
   }
 
+  @ApiOperation({ summary: 'List and paginate systems' })
+  @ApiQuery({
+    name: 'page',
+    description: 'Page to be returned',
+  })
+  @ApiQuery({
+    name: 'orderBy',
+    description: 'Order by field',
+  })
+  @ApiOkResponse({
+    description: 'success',
+    type: PaginationDto,
+  })
   @Get()
   findMany(
     @Query()
-    params: PaginateQueryDto,
+    query: PaginationQueryDto,
     @Req() req: Request,
   ) {
-    return this.service.findManyAndPaginate(params, req.path);
+    return this.service.findManyAndPaginate(query, req.path);
   }
 
+  @ApiOperation({ summary: 'Find one system' })
+  @ApiNotFoundResponse({
+    description: 'not found',
+  })
+  @ApiOkResponse({
+    description: 'success',
+    type: SystemDto,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.service.findOne(id);
